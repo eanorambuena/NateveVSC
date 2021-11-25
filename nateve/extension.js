b2d9e52ca20d1a22ddea9fed6545f3451d4ab861
eanorambuena <eanorambuena@uc.cl>
2021-11-25 20:37:41 -0300

    Config extension

---
+++ nateve/extension.js
@@ -0,0 +1,19 @@
+const vscode = require("vscode");
+
+module.exports = {
+  activate,
+  //deactivate,
+};
+
+function activate(context) {
+  // This must match the command property in the package.json
+  const commandID = "nateve.run";
+  let disposable = vscode.commands.registerCommand(commandID, sayHello);
+  context.subscriptions.push(disposable);
+}
+
+function sayHello() {
+  vscode.window.showInformationMessage("Hello World!");
+}
+
+//function deactivate() {}
